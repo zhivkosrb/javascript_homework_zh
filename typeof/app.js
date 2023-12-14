c72cd7bb4d48b947f4b6e1d9863e3327fc91c826
zhivkosrb <150180608+zhivkosrb@users.noreply.github.com>
2023-12-14 03:30:44 +0100

    3 vo edno
    najdolu ima komentarche

---
+++ typeof/app.js
@@ -0,0 +1,51 @@
+console.log("it's alive!!!");
+/*function typeChecking(testSubject) {
+  let whatType = typeof testSubject;
+  console.log(`The type of "${testSubject}" is ${whatType}`);
+  return whatType;
+}
+typeChecking(1305);
+typeChecking(undefined);
+typeChecking(null);
+typeChecking(false);
+typeChecking("To the next homework");
+*/
+
+/*function convertYears(humanYears) {
+  let dogYears = Number(humanYears * 7);
+  let dogTohuman = Number(humanYears / 7);
+  console.log(`If you were dog your age would be ${dogYears}`);
+  console.log(
+    `And if your dog had same ages as human species it would be ${dogTohuman}`
+  );
+  return dogYears, dogTohuman;
+}
+convertYears(49);
+*/
+/*
+let moiSiparichki = 7500;
+let pin = 3384;
+function cashingOut() {
+  let pinCheck = Number(prompt("Please enter your pin"));
+  if (pinCheck !== pin) {
+    console.log("Incorrect pin");
+  } else {
+    let moneyTaking = Number(
+      prompt("how much money would you like to pull up")
+    );
+    if (moneyTaking <= 0 || isNaN(moneyTaking)) {
+      console.log("Invalid input");
+    } else if (moneyTaking > moiSiparichki) {
+      console.log("you dont have enough money");
+    } else {
+      let moneytaken = moneyTaking;
+      let moneyleft = moiSiparichki - moneyTaking;
+      console.log`You've withdraw ${moneytaken} and left on your bank account ${moneyleft}`;
+      return moneytaken, moneyleft;
+    }
+  }
+}
+console.log(cashingOut());
+*/
+// na poslednava na kraj verojatno ima neshto zeznato \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// ne mi teknuvashe otprvo taka da mackucka si potpomognav so Bing \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
